024ccb938fac44a7c14bc0c97524005491beb5b9
JadenBui <anhbui11211@gmail.com>
2020-04-17 16:03:20 +1000

    del

--- App3/src/User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-    private String userName;
-    private String password;
-    public User(String user, String pass){
-        userName = user;
-        password = pass;
-    }
-}
