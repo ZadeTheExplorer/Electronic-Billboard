c4cd26a2ebd137206e983d8979ea6842a2111547
Zad3TheExplorer <hatrisy@gmail.com>
2020-04-17 13:22:45 +0700

    adding user.java in billboard package

---
+++ src/User.java
@@ -0,0 +1,12 @@
+package Billboard;
+
+
+public class User {
+    private String userName;
+    private String password;
+    public User(String user, String pass) {
+        userName = user;
+        password = pass;
+
+    }
+}
