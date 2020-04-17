51bb52f45ab4c2a164d6a134de6e797fe93edece
JadenBui <anhbui11211@gmail.com>
2020-04-17 15:13:04 +1000

    classcreate

---
+++ App3/src/User.java
@@ -0,0 +1,8 @@
+public class User {
+    private String userName;
+    private String password;
+    public User(String user, String pass){
+        userName = user;
+        password = pass;
+    }
+}
