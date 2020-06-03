f6495aa3d18be1067666d6ca2f4d09438f08d954
EdwardDuong <54650436+EdwardDuong@users.noreply.github.com>
2020-06-03 22:23:27 +1000

    Revert "Revert "Revert "123"""
    This reverts commit 8cff14ebc7ec0cb77d6c5775e4a948f446ec464c.

--- src/Billboard/User.java
+++ src/Billboard/User.java
@@ -4,21 +4,22 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class User {
     private String userName;
-    private String passWord;
     private ArrayList<String> permission;
     private String privilege;
+    private String salt;
+    private String hashPass;
 
-    public User(String name, String password, ArrayList<String> permission, String privilege) throws Exception{
+    public User(String name, String hashPass, String salt, ArrayList<String> permission, String privilege) throws Exception{
         userName = name;
-        passWord = password;
+        this.hashPass = hashPass;
+        this.salt = salt;
         this.permission = new ArrayList<>(permission.size());
         this.permission.addAll(permission);
-        if(privilege.compareTo("") == 0 || privilege == null){
-            throw new Exception("User must have privilege");
-        }this.privilege = privilege;
+        this.privilege = privilege;
     }
 
     public String getUserName() {
@@ -29,15 +30,11 @@ public class User {
         this.userName = userName;
     }
 
-    public String getPassWord() {
-        return passWord;
-    }
+    public String getSalt(){return salt;}
 
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
+    public String getHashPass(){return hashPass;}
 
-    public boolean checkPassword(String enteredPass) {return enteredPass.compareTo(passWord) == 0; }
+    public boolean checkPassword(String enteredPass) {return enteredPass.compareTo(hashPass) == 0; }
 
     public ArrayList<String> getPermission() {
         if(privilege.toUpperCase().compareTo("CREATE BILLBOARDS") == 0){
@@ -70,48 +67,42 @@ public class User {
         this.privilege = privilege;
     }
 
-    public String saltPassWord(String passWord) throws Exception{
-        String algorithm = "MD5";
-        byte[] salt = createSalt();
-        String saltData = bytesToStringHex(salt);
-        return generateHashPass(passWord,algorithm,salt);
+    //Reference the week 9
+    //create a new hashed password
+    public static String hashedPassword (String inputPassword) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        byte[] hashedPassword = md.digest(inputPassword.getBytes());
+
+        return byteToString(hashedPassword);
     }
 
-    //Change password into salt hash
-    private static String generateHash(String passWord, String algorithm) throws NoSuchAlgorithmException {
+    // create a new salt
+    public static String createSalt(){
+        Random rng = new Random();
+        byte[] saltBytes = new byte[32];
+        rng.nextBytes(saltBytes);
+        String saltString = byteToString(saltBytes);
 
-        MessageDigest digest = MessageDigest.getInstance(algorithm);
-        digest.reset();
-        byte[] hash = digest.digest(passWord.getBytes());
-        return bytesToStringHex(hash);
+        return saltString;
     }
 
-    //Change password into salt hash
-    private static String generateHashPass(String passWord, String algorithm, byte[] salt) throws NoSuchAlgorithmException {
+    //create a new salted password
+    public static String saltedPassword (String hashedPassword, String salt) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-        MessageDigest digest = MessageDigest.getInstance(algorithm);
-        digest.reset();
-        digest.update(salt);
-        byte[] hash = digest.digest(passWord.getBytes());
-        return bytesToStringHex(hash);
+        String saltedPassword = byteToString(md.digest((hashedPassword + salt).getBytes()));
+
+        return saltedPassword;
     }
 
-    private final static char[] hexArray = "0123456789ABCDE".toCharArray();
-
-    public static String bytesToStringHex(byte[] bytes){
-        char[] hexChars = new char[bytes.length * 2];
-        for(int i = 0; i < bytes.length; i++){
-            int j = bytes[i] & 0xFF;
-            hexChars[i * 2] = hexArray[j>>>4];
-            hexChars[i * 2 + 1] = hexArray[j & 0x0F];
+    // turning byte to string
+    private static String byteToString(byte[] hash){
+        StringBuffer sb = new StringBuffer();
+        for (byte b : hash) {
+            sb.append(String.format("%02x", b & 0xFF));
         }
-        return new String(hexChars);
-    }
 
-    public static byte[] createSalt(){
-        byte[] bytes = new byte[20];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(bytes);
-        return bytes;
+        return sb.toString();
     }
 }
